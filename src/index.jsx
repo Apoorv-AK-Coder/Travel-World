e5a2c6b4f4bb8958e31a9345d6162b9e9fca8997
Apoorv Agarwal <150249267+Apoorv-AK-Coder@users.noreply.github.com>
2024-10-23 04:02:55 +0000

    dv ermjknbm jbdjknhkfd

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,8 @@
 // import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ReactDOM from "react-dom/client";
 import React from 'react';
-
 import NewApp from "./newApp.jsx";
+
 // import AboutApp from "./AboutApp.jsx";
 import './css/style.css';
 import './css/index.css';
